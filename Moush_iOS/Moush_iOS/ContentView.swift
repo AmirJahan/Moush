0cef3323a13194f4ce33be8c772e4bc5966fac9e
Amir J <amir.jahan@gmail.com>
2023-04-30 12:49:32 -0700

    Added Xcode project

---
+++ Moush_iOS/Moush_iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Moush_iOS
+//
+//  Created by The Odd Institute on 2023-04-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moush_iOS/Moush_iOS/Moush_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moush_iOSApp.swift
+//  Moush_iOS
+//
+//  Created by The Odd Institute on 2023-04-30.
+//
+
+import SwiftUI
+
+@main
+struct Moush_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
