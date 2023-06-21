16fbd7e0326684eba992e7e0c34a3846875ea123
Amir Jahanlou <ajahanlou@sctmac00.vfs.local>
2023-06-21 14:44:33 -0700

    This is the branch that Melissa had made. Amir has made some changes to make it simpler. Ready to merge

--- Moush_iOS/Moush_iOS/TextEditor.swift
+++ Moush_iOS/Moush_iOS/TextEditor.swift
@@ -1,84 +1,84 @@
-//
-//  TextEditor.swift
-//  Moush_iOS
-//
-//  Created by Melissa Osorio Tavera on 2023-05-17.
-//
-
-import Foundation
 import SwiftUI
 
-struct TextAttrView: View {
-    @State private var inputSentence: String = ""
-    let colorArray: [Color] = [.red, .green, .blue, .orange, .purple] // Example color array
+struct TextAttrView: View
+{
+    @State
+    var inputSentence: String = "Hello"
     
-    var coloredText: Text {
-        let hashtags = findHashtags(in: inputSentence)
-        return coloredTextForHashtags(in: inputSentence, hashtags: hashtags)
-    }
+    let colorArray: [Color] = [.red, .green, .blue, .orange, .purple]
+  
+
     
-    var body: some View {
-        VStack {
+    var body: some View
+    {
+        VStack
+        {
             Text("Input Sentence:")
                 .font(.headline)
+            
+            
+            
             ZStack(alignment: .topLeading)
             {
                 TextEditor(text: $inputSentence)
+                    .frame(height: 96)
                     .padding()
                     .foregroundColor(.white)
-                coloredText
-                    .padding()
+                    .font(.system(size: 24))
+//                    .background(.gray)
+                
+                HStack
+                {
+                    coloredTextForHashtags()
+                    //                    .frame(height: 96)
+//                        .background(.red)
+                    
+                        .font(.system(size: 24))
+                    
+                        .padding(.leading, 20)
+                        .padding(.top, 24)
+                }
             }
         }
     }
+
     
-    func findHashtags(in sentence: String) -> [String] {
-        let words = sentence.components(separatedBy: CharacterSet.whitespacesAndNewlines)
-        
-        var hashtags: [String] = []
-        
-        for word in words {
-            if word.hasPrefix("#") {
-                hashtags.append(word)
-            }
-        }
-        
-        return hashtags
-    }
-    
-    func coloredTextForHashtags(in sentence: String, hashtags: [String]) -> Text {
-        let words = sentence.components(separatedBy: CharacterSet.whitespacesAndNewlines)
-        
+    func coloredTextForHashtags() -> Text
+    {
+        let words = inputSentence.components(separatedBy: CharacterSet.whitespacesAndNewlines)
         var coloredText = Text("")
         
-        for word in words {
-            if word.hasPrefix("#") {
+        var colIndex = 0
+        
+        for word in words
+        {
+            if word.hasPrefix("#")
+            {
+                // it's a hashtag
                 let hashtag = word
-                let color = colorForHashtag(hashtag)
+                
+                // let's find the color we want
+//                let color = colorForHashtag(hashtag)
+                let color = colorArray[colIndex]
                 coloredText = coloredText + Text(word + " ").foregroundColor(color)
-            } else {
+                
+                colIndex += 1
+                colIndex %= colorArray.count
+            } else
+            {
                 coloredText = coloredText + Text(word + " ")
             }
         }
         
         return coloredText
     }
-    
-    func colorForHashtag(_ hashtag: String) -> Color {
-        let index = findHashtags(in: inputSentence).firstIndex(of: hashtag) ?? 0
-        let colorIndex = index % colorArray.count
-        return colorArray[colorIndex]
-    }
 }
 
 
-/// extension to make applying AttributedString even easier
-extension Text {
-    init(_ string: String, configure: ((inout AttributedString) -> Void)) {
-        var attributedString = AttributedString(string) /// create an `AttributedString`
-        configure(&attributedString) /// configure using the closure
-        self.init(attributedString) /// initialize a `Text`
+
+
+struct TextAttrView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextAttrView()
     }
 }
-
-
